302fd8079cdc4b245f3c47ca5405203edaf9af71
Jatin-Jangraa <jatin012468@gmail.com>
2025-12-31 09:02:39 +0530

    Remove API key from Gemini.js
    Remove sensitive API key from Gemini.js

--- src/assets/components/config/Gemini.js
+++ src/assets/components/config/Gemini.js
@@ -6,7 +6,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "AIzaSyALKeScMD5Hlz19DRQRFGzKm8eaEWG_ovc";
+  const apiKey = "";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
